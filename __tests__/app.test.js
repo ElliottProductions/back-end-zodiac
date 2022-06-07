1116e5f6fd093f2e3f3f07b78e682ba6795ea664
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-07 10:49:32 -0700

    1st test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,19 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodiacs } = require('../data/zodiacs');
+
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/zodaics should return a list of zodiacs', async () => {
+    const response = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((sign) => { return { id: sign.id, name: sign.name };
+    });
+
+    expect(response.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
